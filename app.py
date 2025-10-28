3a6c474ef0061311fd88d6be73b8ee6205173768
beatrizbzack <beatrizbz@al.insper.edu.br>
2025-10-28 11:26:45 -0300

    add: subheader

--- app.py
+++ app.py
@@ -2,9 +2,10 @@
 import streamlit as st
 from analyzer import analyze_url
 
-st.set_page_config(page_title="Detector de Phishing", page_icon="🎣")
+st.set_page_config(page_title="AF TecHack - Detector de Phishing", page_icon="🎣")
 
 st.title("🎣 Ferramenta de Detecção de Phishing")
+st.subheader("Por: Beatriz Borges Zackiewicz")
 st.write("Insira uma URL para verificar possíveis sinais de phishing:")
 
 url = st.text_input("URL:", placeholder="https://exemplo.com")
